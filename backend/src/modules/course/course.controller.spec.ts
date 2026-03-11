c86484eb4aaa7c866e279fd44176a6fa626169be
Suman Yadav <xisense78@gmail.com>
2026-03-11 17:09:42 +0545

    test DO

--- backend/src/modules/course/course.controller.spec.ts
+++ backend/src/modules/course/course.controller.spec.ts
@@ -12,6 +12,8 @@ describe('CourseController', () => {
     controller = module.get<CourseController>(CourseController);
   });
 
+  
+
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
